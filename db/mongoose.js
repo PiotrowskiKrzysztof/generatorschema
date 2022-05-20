c97d30722442028cc60736832831d12271f5dc70
Krzysztof <47855045+PiotrowskiKrzysztof@users.noreply.github.com>
2022-05-20 13:13:40 +0200

    try config heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
 const { database } = require('../config');
 
-mongoose.connect(database, { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(database, { useUnifiedTopology: true, useNewUrlParser: true }, (error, client) => {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log('db connected');
+    }
+});
