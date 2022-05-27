edc9ec034a8843b12a41f2b53555e42411d0f138
Krzysztof <47855045+PiotrowskiKrzysztof@users.noreply.github.com>
2022-05-27 15:48:40 +0200

    fix mongoose

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,10 +1,4 @@
 const mongoose = require('mongoose');
 const { database } = require('../config');
 
-mongoose.connect(database, { useUnifiedTopology: true, useNewUrlParser: true }, (error, client) => {
-    if(error) {
-        console.log(error);
-    } else {
-        console.log('db connected');
-    }
-});
+mongoose.connect(database, { useUnifiedTopology: true, useNewUrlParser: true });
